462b43848a5e264d11972f3ac7320ce3a02d3246
CharlesLG7580 <459098348@qq.com>
2020-10-20 09:35:14 +0800

    用了库函数所以效率不是很高

---
+++ 剑指offer40最小的k个数.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+vector<int> sorr(vector<int> & arr,int k)
+{
+	vector<int> res;
+	sort(arr.begin(),arr.end());  //´Ë´¦µÄÅÅÐò 
+	for(int i=0;i<k;i++)
+	{
+		res.push_back(arr[i]);
+	}
+	for(int i=0;i<res.size();i++)
+	{
+		cout<<res[i];
+	}
+	
+} 
+int main()
+{
+	vector<int> ar={0,1,2,1};
+	int k=1;
+	sorr(ar,k);
+}
